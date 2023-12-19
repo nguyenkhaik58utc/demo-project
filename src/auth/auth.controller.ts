ee23df03746a1284edd612c7f478a2a32fe3cc55
nguyenkhaik58utc <nguyenkhaik58utc@gmail.com>
2023-12-19 10:16:35 +0700

    update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,30 @@
 import { AuthService } from './auth.service';
-import { Controller, Post, Req, Body } from '@nestjs/common';
+import { Controller, Post, Req, Body, Get, Param, Query } from '@nestjs/common';
 import { AuthDTO } from './dto';
+import { query } from 'express';
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {
   }
+
   @Post("register")
   async register(@Body() request: AuthDTO){
-      console.log(request);
       return await this.authService.register(request)
   }
+
+  @Post("get-list-user")
+  async getListUser(){
+    return await this.authService.getListUser()
+  }
+
+  @Get("get-user")
+  async getUser(
+    @Query("email") email: string,
+    @Query("name") name: string
+  ){
+    return await this.authService.getUser(email, name)
+  }
+  
   @Post("login")
   login(){
       return this.authService.login()
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,17 +2,88 @@ import { PrismaService } from './../prisma/prisma.service';
 import { Injectable } from "@nestjs/common";
 import * as argon from 'argon2'
 import { AuthDTO } from './dto';
+import { contains } from 'class-validator';
 @Injectable({})
 export class AuthService{
     constructor(private prismaService:PrismaService){
 
     }
-    register(authDTO: AuthDTO){
-        const hash = argon.hash(authDTO.password);
-        return {
-            message: hash
+    async register(authDTO: AuthDTO){
+        try {
+            const hash = await argon.hash(authDTO.password);
+            const user = await this.prismaService.user.create({
+                data : {
+                    email: authDTO.email,
+                    hashedPassword: hash,
+                    firstName: "test",
+                    lastName: "123"
+                },
+                select:{
+                    id: true,
+                    createdAt: true
+                }
+            })
+            return {
+                message: user
+            }
+        } catch (error) {
+            return {
+                message: error.message
+            }
         }
     }
+
+    async getListUser(){
+        try {
+            return await this.prismaService.user.findMany({
+                select:{
+                    id: true,
+                    lastName: true,
+                    firstName: true,
+                    email: true,
+                    createdAt: true
+                }
+            })
+        } catch (error) {
+            return error.message
+        }
+    }
+
+    async getUser(email: string, name: string){
+        var data = {
+            id : 0,
+            email: "",
+            name: ""
+        }
+        try {
+            var user = await this.prismaService.user.findUnique({
+                select:{
+                    id: true,
+                    email: true,
+                    firstName: true,
+                    lastName: true
+                },
+                where:{
+                        email: "test@fpt.com"
+                }
+            });
+            if(user){
+                data.id = user.id;
+                data.email = user.email;
+                data.name = `${user.firstName} ${user.lastName}`;
+            }
+            return {
+                data: data,
+                message: "success"
+            }
+        } catch (error) {
+            return {
+                data: data,
+                message: error.message
+            }
+        }
+    }
+
     login(){
         return {
             message: "login"
